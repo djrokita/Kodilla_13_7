0ec0d75ebd9f12e16024f28caf61ecd4d08bdb40
Krzych <krzych.pierzchala@gmail.com>
2017-11-02 07:26:14 +0100

    Project ready to push

---
+++ zadanie.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var colors = require('colors');
+
+fs.readdir(__dirname, function(err, files)
+{
+	console.log(files);
+	fs.writeFile('./myPath.txt', files, function(err, data) {
+		if (err) throw err;
+		console.log('Zapisano do pliku'.green);
+	});
+});
+
+fs.readFile('./myPath.txt', 'utf-8', function(err, data) {
+	console.log('Dane po zapisie'.blue);
+	console.log(data);
+
+});
+
